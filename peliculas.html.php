feb373ea450879bcdc860cbaed6f17e44a1a5ef3
pabsk8 <jimenezcevallosp@gmail.com>
2021-07-11 17:16:58 -0500

    Este es el primer sistema commit

---
+++ peliculas.html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear peliculas</title>
+</head>
+<body>
+    <h1>Esta es la pagina para crear peliculas</h1>
+    <label>Nombre:</label><br>
+    <input type="text"><br>
+
+    <label>Descripcion:</label><br>
+    <input type="text" style="width:200px;height:175px"><br>
+    <label>Categoria:</label><br>
+    <input type="text"><br>
+
+    <button>enviar formulario</button>
+</body>
+</html>
